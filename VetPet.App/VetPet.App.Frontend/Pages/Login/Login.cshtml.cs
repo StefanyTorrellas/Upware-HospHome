a0bffe2b3d0b7201d54f4a09108565cbf4ef72b5
Miguel C <mc102579@gmail.com>
2021-09-21 12:17:48 -0400

    login system

--- VetPet.App/VetPet.App.Frontend/Pages/Login/Login.cshtml.cs
+++ VetPet.App/VetPet.App.Frontend/Pages/Login/Login.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace VetPet.App.Frontend.Pages
+namespace VetPet.App.Frontend.Pages.Index
 {
     public class LoginModel : PageModel
     {
