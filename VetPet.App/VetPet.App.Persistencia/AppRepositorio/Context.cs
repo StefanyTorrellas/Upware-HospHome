d4add70d9ebd023040814d7e064e23704de2464c
Miguel C <mc102579@gmail.com>
2021-09-23 16:57:29 -0400

    User

--- VetPet.App/VetPet.App.Persistencia/AppRepositorio/Context.cs
+++ VetPet.App/VetPet.App.Persistencia/AppRepositorio/Context.cs
@@ -12,6 +12,7 @@ namespace VetPet.App.Persistencia
         public DbSet<Cita> Citas{get; set;}
         public DbSet<HistoriaClinica> HistoriasClinicas {get; set;}
         public DbSet<Usuario> Usuarios {get; set;}
+        public DbSet<Admin> Admins {get; set;}
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
