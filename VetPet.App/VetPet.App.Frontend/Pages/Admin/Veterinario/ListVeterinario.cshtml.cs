039da824ca5cc0a9fc82ef44e421f41f41e41956
Miguel C <mc102579@gmail.com>
2021-09-23 19:56:19 -0400

    Admin

---
+++ VetPet.App/VetPet.App.Frontend/Pages/Admin/Veterinario/ListVeterinario.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VetPet.App.Frontend.Pages
+{
+    public class ListVeterinarioModel : PageModel
+    {
+
+        private readonly IRepositorioVeterinario repositorioVeterinario;
+        public IEnumerable<Veterinario>veterinarios;
+        public ListVeterinarioModel(IRepositorioVeterinario repositorioVeterinario) {
+            this.repositorioVeterinario = repositorioVeterinario;
+        }
+        public void OnGet()
+        {
+            veterinarios = repositorioVeterinario.getAllVeterinarios();
+        }
+    }
+}
