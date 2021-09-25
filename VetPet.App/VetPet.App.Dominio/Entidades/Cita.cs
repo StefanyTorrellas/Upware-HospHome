34e674d8755b11367cdb06401970397c5aa36177
EDWIN HERNAN GUANARAN <ehguanarane@unadvirtual.edu.co>
2021-09-25 00:35:35 -0500

    Cita

--- VetPet.App/VetPet.App.Dominio/Entidades/Cita.cs
+++ VetPet.App/VetPet.App.Dominio/Entidades/Cita.cs
@@ -5,8 +5,8 @@ namespace VetPet.App.Dominio {
     public class Cita
     {
         public int id {get; set;} 
-        public string fecha {get; set;}
-        public string hora {get; set;}
+        public String fecha {get; set;}
+        public String hora {get; set;}
         public int mascota_id {get; set;}
         
     }
