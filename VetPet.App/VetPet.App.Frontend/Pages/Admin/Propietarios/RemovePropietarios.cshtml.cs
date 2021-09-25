f3a228f5e9ce82668dcc8e09982e8655ced9a8be
Miguel C <mc102579@gmail.com>
2021-09-24 20:28:41 -0400

    implementando frontend

--- VetPet.App/VetPet.App.Frontend/Pages/Admin/Propietarios/RemovePropietarios.cshtml.cs
+++ VetPet.App/VetPet.App.Frontend/Pages/Admin/Propietarios/RemovePropietarios.cshtml.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VetPet.App.Dominio;
+using VetPet.App.Persistencia;
 
 namespace VetPet.App.Frontend.Pages
 {
     public class RemovePropietariosModel : PageModel
     {
-        public void OnGet()
+        
+        private readonly IRepositorioPropietario repositorioPropietario;
+        public Propietario propietario {get; set;}
+
+        public RemovePropietariosModel(IRepositorioPropietario repositorioPropietario) {
+            this.repositorioPropietario = repositorioPropietario;
+        }
+
+        public void OnGet(int cedula)
         {
+            propietario = repositorioPropietario.getPropietario(cedula);
+        }
+
+        public IActionResult OnPost(int cedula) {
+            repositorioPropietario.removePropietario(cedula);
+            return RedirectToPage("/Admin/Propietarios/ListPropietarios");
         }
     }
 }
--- VetPet.App/VetPet.App.Frontend/Pages/Admin/Usuarios/ListUsuarios.cshtml.cs
+++ VetPet.App/VetPet.App.Frontend/Pages/Admin/Usuarios/ListUsuarios.cshtml.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VetPet.App.Dominio;
+using VetPet.App.Persistencia;
 
 namespace VetPet.App.Frontend.Pages
 {
     public class ListUsuariosModel : PageModel
     {
+        private readonly IRepositorioUsuario repositorioUsuario;
+        public IEnumerable<Usuario> usuarios;
+        public ListUsuariosModel(IRepositorioUsuario repositorioUsuario) {
+            this.repositorioUsuario = repositorioUsuario;
+        }
+
         public void OnGet()
         {
+            usuarios = repositorioUsuario.getAllUsuarios();
         }
     }
 }
